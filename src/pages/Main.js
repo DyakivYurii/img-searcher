f4cb9806f456ff6942a91aaba96ff96df0cf19c7
Yurii <dyakivyurii@gmail.com>
2019-05-19 11:15:49 +0300

    Gallery

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,11 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Searcher from '../components/Searcher';
+import Gallery from '../components/Gallery';
 
 const Main = (props) => {
   return (
     <Wrapper>
       <Searcher />
+      <Gallery />
     </Wrapper>
   );
 };
